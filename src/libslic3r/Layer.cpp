6d88579ff115e79a73b16d913150fbdd5a77214b
Nico <cd@cd.cd>
2025-08-26 11:23:10 +0200

    Again

--- src/libslic3r/Layer.cpp
+++ src/libslic3r/Layer.cpp
@@ -660,6 +660,8 @@ inline bool has_compatible_layer_regions(const PrintRegionConfig &config, const 
            config.thin_walls                                            == other_config.thin_walls &&
            config.external_perimeters_first                             == other_config.external_perimeters_first &&
            config.enable_injection_molding_order                        == other_config.enable_injection_molding_order    &&
+           config.enable_injection_molding_temp_boost                   == other_config.enable_injection_molding_temp_boost    &&
+           config.injection_molding_temp_boost                          == other_config.injection_molding_temp_boost    &&
            config.reverse_internal_perimeters                           == other_config.reverse_internal_perimeters    &&
            config.infill_overlap                                        == other_config.infill_overlap &&
            has_compatible_dynamic_overhang_speed(config, other_config);
