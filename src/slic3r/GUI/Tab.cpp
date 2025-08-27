cbdfa150ebb513a5cb6fb8b32530019b4a41d175
Nico <cd@cd.cd>
2025-08-27 20:42:53 +0200

    again

--- src/slic3r/GUI/Tab.cpp
+++ src/slic3r/GUI/Tab.cpp
@@ -1481,7 +1481,7 @@ void TabPrint::build()
         optgroup->append_single_option_line("reverse_internal_perimeters", category_path + "reverse_internal_perimeters");
         optgroup->append_single_option_line("reverse_internal_perimeters_at", category_path + "reverse_internal_perimeters_at");
 
-        optgroup->append_single_option_line("temperatureOffsetLayers", category_path + "temperatureoffsetlayers");
+        optgroup->append_single_option_line("TemperatureOffsetLayers", category_path + "temperatureoffsetlayers");
         //optgroup->append_single_option_line("only_one_perimeter_first_layer", category_path + "only-one-perimeter-first-layer");
 
         optgroup->append_single_option_line("external_perimeters_first", category_path + "external-perimeters-first");
