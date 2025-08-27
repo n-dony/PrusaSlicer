26a10a0e7d9c62c9bbcb5a465a331c7e71624844
Nico <cd@cd.cd>
2025-08-27 18:31:17 +0200

    fix

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -56,7 +56,7 @@ static t_config_enum_names enum_names_from_keys_map(const t_config_enum_values &
     for (const auto& kvp : enum_keys_map)
         names[kvp.second] = kvp.first;
     return names;
-}
+};
 
 static const t_config_enum_values s_keys_map_TemperatureOffsetLayers = {
     { "all",            static_cast<int>(TemperatureOffsetLayers::All) },
